43c392b50b71b159744ebc0ec693e1ae3c947376
VladiG1984 <vladig1984@gmail.com>
2018-08-13 16:26:45 +0300

    Create initial setup.

---
+++ src/main/java/com/telerikacademy/ngpuppies/NgPuppiesApplication.java
@@ -0,0 +1,12 @@
+package com.telerikacademy.ngpuppies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgPuppiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgPuppiesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/telerikacademy/ngpuppies/NgPuppiesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.telerikacademy.ngpuppies;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NgPuppiesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
