a68d615312f254bedc3fc82660f2a2709377a970
VladiG1984 <vladig1984@gmail.com>
2018-08-20 18:22:37 +0300

    Created AdminService.

---
+++ src/main/java/com/telerikacademy/ngpuppies/services/base/AdminService.java
@@ -0,0 +1,8 @@
+package com.telerikacademy.ngpuppies.services.base;
+
+import com.telerikacademy.ngpuppies.models.Admin;
+
+public interface AdminService extends GenericService<Admin> {
+	
+	void delete(int id);
+}
