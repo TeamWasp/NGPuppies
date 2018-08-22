1b67e98c4977327fcdd6e3eb225d579b1679d70a
VladiG1984 <vladig1984@gmail.com>
2018-08-22 03:45:19 +0300

    Added property isNewUser to model and its table

--- src/main/java/com/telerikacademy/ngpuppies/models/Admin.java
+++ src/main/java/com/telerikacademy/ngpuppies/models/Admin.java
@@ -10,19 +10,24 @@ import javax.validation.constraints.Email;
 public class Admin extends User {
 	
 	@Email
-	@Column(name ="EmailAddress")
+	@Column(name = "EmailAddress")
 	private String emailAddress;
+	
+	@Column(name = "IsNewUser")
+	private boolean isNewUser;
 	
 	public Admin() {}
 	
-	public Admin(String username, String password, Role role, String eik, String emailAddress) {
+	public Admin(String username, String password, Role role, String eik, String emailAddress, Boolean isNewUser) {
 		super(username, password, role, eik);
 		this.emailAddress = emailAddress;
+		this.isNewUser = isNewUser;
 	}
 	
-	public Admin(String username, String password, String eik, String emailAddress) {
+	public Admin(String username, String password, String eik, String emailAddress, Boolean isNewUser) {
 		super(username, password, eik);
 		this.emailAddress = emailAddress;
+		this.isNewUser = isNewUser;
 	}
 	
 	public String getEmailAddress() {
@@ -31,5 +36,13 @@ public class Admin extends User {
 	
 	public void setEmailAddress(String emailAddress) {
 		this.emailAddress = emailAddress;
+	}
+	
+	public boolean getIsNewUser() {
+		return isNewUser;
+	}
+	
+	public void setIsNewUser(Boolean isNewUser) {
+		this.isNewUser = isNewUser;
 	}
 }
