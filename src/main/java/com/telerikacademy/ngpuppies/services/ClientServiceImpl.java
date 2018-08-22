7857437e31afc75122f09a7d2a00e396845a38e3
VladiG1984 <vladig1984@gmail.com>
2018-08-22 17:49:45 +0300

    Added two repositories (BillRepository & SubscriberRepository) and updated getAllBills by userId method in order for it to work properly.

--- src/main/java/com/telerikacademy/ngpuppies/services/ClientServiceImpl.java
+++ src/main/java/com/telerikacademy/ngpuppies/services/ClientServiceImpl.java
@@ -1,21 +1,30 @@
 package com.telerikacademy.ngpuppies.services;
 
 import com.telerikacademy.ngpuppies.models.Bill;
+import com.telerikacademy.ngpuppies.models.Client;
 import com.telerikacademy.ngpuppies.models.Subscriber;
+import com.telerikacademy.ngpuppies.models.User;
+import com.telerikacademy.ngpuppies.repositories.base.BillRepository;
 import com.telerikacademy.ngpuppies.repositories.base.ClientRepository;
+import com.telerikacademy.ngpuppies.repositories.base.SubscriberRepository;
 import com.telerikacademy.ngpuppies.services.base.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ClientServiceImpl implements ClientService {
     private ClientRepository repository;
+    private BillRepository billRepository;
+    private SubscriberRepository subscriberRepository;
 
     @Autowired
-    public ClientServiceImpl(ClientRepository repository) {
+    public ClientServiceImpl(ClientRepository repository, BillRepository billRepository, SubscriberRepository subscriberRepository) {
         this.repository = repository;
+        this.billRepository = billRepository;
+        this.subscriberRepository = subscriberRepository;
     }
 
     @Override
@@ -33,9 +42,22 @@ public class ClientServiceImpl implements ClientService {
         repository.payBill(billId);
     }
 
-    @Override
+    /*@Override
     public List<Bill> getAllBills(int userId) {
         return this.repository.getAllBills(userId);
+    }*/
+    
+    @Override
+    public List<Bill> getAllBills(int userId) {
+        User client = repository.getById(userId);
+        List<Subscriber> subscribers = subscriberRepository.getAll(userId);
+        List<Bill> billsClient = new ArrayList<>();
+        for (Subscriber sub : subscribers) {
+            List<Bill> billsSub = billRepository.getAll(sub.getPhoneNumber());
+            billsClient.addAll(billsSub);
+        }
+        
+        return billsClient;
     }
 
     @Override
