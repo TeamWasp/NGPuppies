5798bb76b32cba0a83ef171821180ca3977a0f80
VladiG1984 <vladig1984@gmail.com>
2018-08-31 02:05:10 +0300

    Implemented UserService & its method.

--- src/main/java/com/telerikacademy/ngpuppies/services/UserServiceImpl.java
+++ src/main/java/com/telerikacademy/ngpuppies/services/UserServiceImpl.java
@@ -3,33 +3,30 @@ package com.telerikacademy.ngpuppies.services;
 
 import com.telerikacademy.ngpuppies.models.User;
 import com.telerikacademy.ngpuppies.repositories.UserSqlRepository;
+import com.telerikacademy.ngpuppies.services.base.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
-import static java.util.Collections.emptyList;
-
 @Service("UserServiceImpl")
-public class UserServiceImpl implements UserDetailsService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     @Autowired
     private UserSqlRepository userRepository;
-
-
+    
     public UserServiceImpl(UserSqlRepository userRepository) {
         this.userRepository = userRepository;
-
     }
-
-
-
+    
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.getByUsername(username);
+    }
+    
+    @Override
+    public User getByUsername(String username) {
         return userRepository.getByUsername(username);
     }
 }
